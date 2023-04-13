bc7319a254859b9023a6ab8b47f99a88f787f5a7
Tomoaki <87553219+Tomoamura@users.noreply.github.com>
2023-04-13 21:09:42 +0900

    NoiseMap.cpp の修正とブランチのテスト

--- NoiseMap.cpp
+++ NoiseMap.cpp
@@ -14,6 +14,7 @@ using namespace std;
 // Parameters
 const int N = 17; // Number of lattice
 const double L = (double) N;
+double t = 0.0;
 double dt = 1.e-0;
 
 // for random distribution
@@ -26,7 +27,7 @@ normal_distribution<> dist(0., 1.);
 
 
 // Function prototype declaration
-// vector<vector<vector<vector<vector<double>>>>> noise(double t, const vector<vector<vector<vector<vector<double>>>>> &x);
+vector<vector<vector<vector<vector<double>>>>> noise(double t, const vector<vector<vector<vector<vector<double>>>>> &x);
 
 
 int main()
@@ -47,45 +48,47 @@ int main()
 
   ofstream ofs_n("chaotic_noise.dat");
 
-  vector<vector<vector<vector<vector<double>>>>> dxdw = x;
 
-  // make correlation noise
-  double peak = 4.; // peak point of power spectrum
-  double ksigma = peak * 2. * M_PI / L; // * sigma * exp(t)
-  double dtheta = M_PI / ksigma / L / 5.;
-  int divth = int(M_PI / dtheta);
-  vector<double> divph(divth);
-  vector<double> thetai(divth);
-  vector<double> dphi(divth);
-  vector<double> dOmegai(divth);
+  vector<vector<vector<vector<vector<double>>>>> dxdw = noise(t,x);
 
-  // inner product of coarse-grained vector and position vector
-  double ksx = 0.;
-  vector<vector<double>> Omegalist;
-  for (int n = 0; n < divth; n++) {
-    thetai[n] = (n + 0.5) * dtheta;
-    dphi[n] = M_PI / ksigma / L / sin(thetai[n]) / 5.;
-    divph[n] = int(2. * M_PI / dphi[n]);
-    for (int l = 0; l < divph[n]; l++){
-      double phii = l * dphi[n];
-      Omegalist.push_back({thetai[n], dphi[n], phii, sqrt(dt) * dist(engine)});
-    }
-  }
+//   // make correlation noise
+//   double peak = 5.; // peak point of power spectrum
+//   double ksigma = peak * 2. * M_PI / L; // * sigma * exp(t)
+//   double dtheta = M_PI / ksigma / L / 5.;
+//   int divth = int(M_PI / dtheta);
+//   vector<double> divph(divth);
+//   vector<double> thetai(divth);
+//   vector<double> dphi(divth);
+//   vector<double> dOmegai(divth);
 
-  LOOP{
-    dxdw[i][j][k][0][0] = 0.; // initialize
-    for (int n = 0; n < Omegalist.size(); n++){
-      double thet = Omegalist[n][0];
-      double dpht = Omegalist[n][1];
-      double phit = Omegalist[n][2];
-      double dwt = Omegalist[n][3];
-      double dOmegai = sin(thet) * dtheta * dpht;
-      ksx = (i+0.5) * sin(thet)*cos(phit) + (j+0.5) * sin(thet)*sin(phit) + (k+0.5) * cos(thet);
-      ksx *= ksigma;
-      dxdw[i][j][k][0][0] += 0.5 * sqrt(dOmegai / M_PI) * (cos(ksx) - sin(ksx)) * dwt;
-    }
-  ofs_n << dxdw[i][j][k][0][0] << endl;
-  }
+//   // inner product of coarse-grained vector and position vector
+//   double ksx = 0.;
+//   vector<vector<double>> Omegalist;
+//   for (int n = 0; n < divth; n++) {
+//     thetai[n] = (n + 0.5) * dtheta;
+//     dphi[n] = M_PI / ksigma / L / sin(thetai[n]) / 5.;
+//     divph[n] = int(2. * M_PI / dphi[n]);
+//     for (int l = 0; l < divph[n]; l++){
+//       double phii = l * dphi[n];
+//       Omegalist.push_back({thetai[n], dphi[n], phii, sqrt(dt) * dist(engine)});
+//     }
+//   }
+
+//   LOOP{
+//     dxdw[i][j][k][0][0] = 0.; // initialize
+//     for (int n = 0; n < Omegalist.size(); n++){
+//       double thet = Omegalist[n][0];
+//       double dpht = Omegalist[n][1];
+//       double phit = Omegalist[n][2];
+//       double dwt = Omegalist[n][3];
+//       double dOmegai = sin(thet) * dtheta * dpht;
+//       ksx = (i+0.5) * sin(thet)*cos(phit) + (j+0.5) * sin(thet)*sin(phit) + (k+0.5) * cos(thet);
+//       ksx *= ksigma;
+//       dxdw[i][j][k][0][0] += 0.5 * sqrt(dOmegai / M_PI) * (cos(ksx) - sin(ksx)) * dwt;
+//     }
+//   ofs_n << dxdw[i][j][k][0][0] << endl;
+//   }
+  LOOP ofs_n << dxdw[i][j][k][0][0] << endl;
 
   // ---------------- return elapsed time --------------
   gettimeofday(&tv, &tz);
@@ -95,48 +98,44 @@ int main()
 }
 
 
-// vector<vector<vector<vector<vector<double>>>>> noise(double t, const vector<vector<vector<vector<vector<double>>>>> &x)
-// {
-  // vector<vector<vector<vector<vector<double>>>>> dxdw = x;
+vector<vector<vector<vector<vector<double>>>>> noise(double t, const vector<vector<vector<vector<vector<double>>>>> &x)
+{
+    vector<vector<vector<vector<vector<double>>>>> dxdw = x;
+    // make correlation noise
+    double peak = 5.; // peak point of power spectrum
+    double ksigma = peak * 2. * M_PI / L; // * sigma * exp(t)
+    double dtheta = M_PI / ksigma / L / 5.;
+    int divth = int(M_PI / dtheta);
+    vector<double> divph(divth);
+    vector<double> thetai(divth);
+    vector<double> dphi(divth);
+    vector<double> dOmegai(divth);
 
-  // // make correlation noise
-  // double ksigma = sigma * exp(t) * m;
-  // double dtheta = M_PI / ksigma / L / 5.;
-  // int divth = int(M_PI / dtheta);
-  // vector<double> divph(divth);
-  // vector<double> thetai(divth);
-  // vector<double> dphi(divth);
-  // vector<double> dOmegai(divth);
-  // cout << divth << endl;
+    // inner product of coarse-grained vector and position vector
+    double ksx = 0.;
+    vector<vector<double>> Omegalist;
+    for (int n = 0; n < divth; n++) {
+        thetai[n] = (n + 0.5) * dtheta;
+        dphi[n] = M_PI / ksigma / L / sin(thetai[n]) / 5.;
+        divph[n] = int(2. * M_PI / dphi[n]);
+        for (int l = 0; l < divph[n]; l++){
+        double phii = l * dphi[n];
+        Omegalist.push_back({thetai[n], dphi[n], phii, sqrt(dt) * dist(engine)});
+        }
+    }
 
-  // // inner product of coarse-grained vector and position vector
-  // double ksx = 0.;
-  // vector<vector<double>> Omegalist;
-  // for (int n = 0; n < divth; n++) {
-  //   thetai[n] = (n + 0.5) * dtheta;
-  //   dphi[n] = M_PI / ksigma / L / sin(thetai[n]) / 5.;
-  //   divph[n] = int(2. * M_PI / dphi[n]);
-  //   for (int l = 0; l < divph[n]; l++){
-  //     double phii = l * dphi[n];
-  //     Omegalist.push_back({thetai[n], dphi[n], phii, sqrt(dt) * dist(engine)});
-  //   }
-  // }
-  // cout << Omegalist.size() << endl;
-
-  // LOOP{
-  //   dxdw[i][j][k][0][0] = 0.; // initialize
-  //   for (int n = 0; n < Omegalist.size(); n++){
-  //     double thet = Omegalist[n][0];
-  //     double dpht = Omegalist[n][1];
-  //     double phit = Omegalist[n][2];
-  //     double dwt = Omegalist[n][3];
-  //     double dOmegai = sin(thet) * dtheta * dpht;
-  //     ksx = (i+0.5) * sin(thet)*cos(phit) + (j+0.5) * sin(thet)*sin(phit) + (k+0.5) * cos(thet);
-  //     ksx *= ksigma * dx;
-  //     dxdw[i][j][k][0][0] += 0.5 * sqrt(dOmegai / M_PI) * (cos(ksx) - sin(ksx)) * dwt;
-  //   }
-  // cout << dxdw[i][j][k][0][0] << endl;
-  // }
-
-  // return dxdw;
-// }
+    LOOP{
+        dxdw[i][j][k][0][0] = 0.; // initialize
+        for (int n = 0; n < Omegalist.size(); n++){
+        double thet = Omegalist[n][0];
+        double dpht = Omegalist[n][1];
+        double phit = Omegalist[n][2];
+        double dwt = Omegalist[n][3];
+        double dOmegai = sin(thet) * dtheta * dpht;
+        ksx = (i+0.5) * sin(thet)*cos(phit) + (j+0.5) * sin(thet)*sin(phit) + (k+0.5) * cos(thet);
+        ksx *= ksigma;
+        dxdw[i][j][k][0][0] += 0.5 * sqrt(dOmegai / M_PI) * (cos(ksx) - sin(ksx)) * dwt;
+        }
+    }
+    return dxdw;
+}
