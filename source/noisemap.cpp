8d30671f201b1b8889e574103f07aeb91c027b73
yuichiro <yuichiro.t.tada@gmail.com>
2023-09-21 02:09:43 +0900

    initial commit Makefile

--- source/noisemap.cpp
+++ source/noisemap.cpp
@@ -1,25 +1,50 @@
 #include <sys/time.h>
 #include <iostream>
+#include <fstream>
+#include <random>
+
+#include <chrono>
+#include <thread>
 
 #include "fft.hpp"
 
-int main()
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
+
+// -------------- User may change ------------------
+const double sigma = 0.1; // ksigma = 2pi sigma exp(N) / L, nsigma = sigma exp(N)
+const double dn = 1; // thickness of nsigma sphere shell
+const int NL = pow(2,5); // box size L
+const double dN = 0.01; // e-folds step
+const std::string filename = "noisedata/noisemap_";
+// -------------------------------------------------
+
+std::vector<double> dwlist(double N);
+
+// random distribution
+std::random_device seed;
+std::mt19937 engine(seed());
+std::normal_distribution<> dist(0., 1.);
+
+// useful macro
+#define LOOP for(int i = 0; i < NL; i++) for(int j = 0; j < NL; j++) for(int k = 0; k < NL; k++)
+
+
+int main(int argc, char* argv[]) 
 {
-  int Num = pow(2, 7);
-  double kx1 = 10;
-  double kx2 = 29;
-  double ky1 = 21;
-  double ky2 = 1;
-  double kz1 = 3;
-  double kz2 = 18;
-  std::vector<std::vector<std::vector<std::complex<double>>>> signal(Num, std::vector<std::vector<std::complex<double>>>(Num, std::vector<std::complex<double>>(Num, 0)));
-  for (int i = 0; i < Num; i++) {
-    for (int j = 0; j < Num; j++) {
-      for (int k = 0; k < Num; k++) {
-	signal[i][j][k] = 1./Num/Num/Num*(std::polar(1., 2*M_PI*(kx1*i+ky1*j+kz1*k)/Num) + std::polar(1., 2*M_PI*(kx2*i+ky2*j+kz2*k)/Num));
-      }
-    }
+  if (argc!=2) {
+    std::cout << "Specify the noise file number correctly." << std::endl;
+    return -1;
   }
+
+  std::ofstream ofs(filename + std::string(argv[1]) + std::string(".dat"));
+  if (ofs.fail()) {
+    std::cout << "The noise file couldn't be opend." << std::endl;
+    return -1;
+  }
+
   
   // ---------- start timer ----------
   struct timeval Nv;
@@ -30,37 +55,32 @@ int main()
   before = (double)Nv.tv_sec + (double)Nv.tv_usec * 1.e-6;
   // --------------------------------------
 
+#ifdef _OPENMP
+  std::cout << "OpenMP : Enabled (Max # of threads = " << omp_get_max_threads() << ")" << std::endl;
+#endif
 
-  // FFT
-  std::vector<std::vector<std::vector<std::complex<double>>>> spectrumf = fft(signal);
+  int totalstep = ceil(log(NL/sigma)/dN), count = 0;
+  std::vector<std::vector<double>> noisedata(totalstep, std::vector<double>(NL*NL*NL,0));
   
-  // ---------- stop timer ----------
-  gettimeofday(&Nv, &Nz);
-  after = (double)Nv.tv_sec + (double)Nv.tv_usec * 1.e-6;
-  std::cout << "FFT " << after - before << " sec." << std::endl;
-  // -------------------------------------
-  
-  gettimeofday(&Nv, &Nz);
-  before = (double)Nv.tv_sec + (double)Nv.tv_usec * 1.e-6;
-
-  
-  // DFT
-  std::vector<std::vector<std::vector<std::complex<double>>>> spectrumd = dft(signal);
-  
-  gettimeofday(&Nv, &Nz);
-  after = (double)Nv.tv_sec + (double)Nv.tv_usec * 1.e-6;
-  std::cout << "DFT " << after - before << " sec." << std::endl;
-  
-  
-  for (int i = 0; i < signal.size(); i++) {
-    for (int j = 0; j < signal[0].size(); j++) {
-      for (int k = 0; k < signal[0][0].size(); k++) {
-	if (abs(spectrumf[i][j][k]) > 1e-10) {
-	  std::cout << i << ' ' << j << ' ' << k << ' ' << abs(spectrumf[i][j][k]) << std::endl;
-	}
-      }
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+  for (int i=0; i<totalstep; i++) {
+    noisedata[i] = dwlist(i*dN);
+#ifdef _OPENMP
+#pragma omp critical
+#endif
+    {
+      count++;
+      std::cout << "\r" << std::setw(3) << 100*count/totalstep << "%" << std::flush;
     }
   }
-  
-  return 0;
+  std::cout << std::endl;
 }
+
+
+std::vector<double> dwlist(double N) {
+  std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  return std::vector<double>{1,2};
+}
+
