b10331f998c8751d837233ecccb48bd8efc192cf
Yuichiro Tada <yuichiro.t.tada@gmail.com>
2023-10-06 14:27:15 +0900

    biasmap.cpp WIP

--- source/biasmap.cpp
+++ source/biasmap.cpp
@@ -25,9 +25,7 @@ const std::string filename = "biasdata/biasmap.dat";
 
 const std::complex<double> II(0,1);
 
-std::vector<double> dwlist(double N);
-bool realpoint(int nx, int ny, int nz, int Num); // judge real point
-bool complexpoint(int nx, int ny, int nz, int Num); // judge independent complex point
+std::vector<double> biaslist(double N);
 bool innsigma(int nx, int ny, int nz, int Num, double nsigma, double dn); // judge if point is in nsigma sphere shell
 
 // random distribution
@@ -37,3 +35,112 @@ std::normal_distribution<> dist(0., 1.);
 
 // useful macro
 #define LOOP for(int i = 0; i < NL; i++) for(int j = 0; j < NL; j++) for(int k = 0; k < NL; k++)
+
+
+int main() 
+{
+  std::ofstream ofs(filename);
+  if (ofs.fail()) {
+    std::cout << "The bias file couldn't be opened." << std::endl;
+    return -1;
+  }
+
+  
+  // ---------- start timer ----------
+  struct timeval Nv;
+  struct timezone Nz;
+  double before, after;
+  
+  gettimeofday(&Nv, &Nz);
+  before = (double)Nv.tv_sec + (double)Nv.tv_usec * 1.e-6;
+  // --------------------------------------
+
+#ifdef _OPENMP
+  std::cout << "OpenMP : Enabled (Max # of threads = " << omp_get_max_threads() << ")" << std::endl;
+#endif
+
+  std::cout << "Box size : " << NL << std::endl;
+  
+  int totalstep = ceil(log((NL/2-1)/sigma)/dN), count = 0;
+  std::vector<std::vector<double>> biasdata(totalstep, std::vector<double>(NL*NL*NL,0));
+  
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+  for (int i=0; i<totalstep; i++) {
+    biasdata[i] = biaslist(i*dN);
+#ifdef _OPENMP
+#pragma omp critical
+#endif
+    {
+      count++;
+      std::cout << "\rBiasGenerating : " << std::setw(3) << 100*count/totalstep << "%" << std::flush;
+    }
+  }
+  std::cout << std::endl;
+  
+  for (size_t i=0; i<biasdata[0].size(); i++) {
+    for (size_t n=0; n<biasdata.size(); n++) {
+      ofs << biasdata[n][i] << ' ';
+    }
+    ofs << std::endl;
+    std::cout << "\rExporting : " << std::setw(3) << 100*i/biasdata[0].size() << "%" << std::flush;
+  }
+  std::cout << "\rExporting : 100%" << std::endl;
+
+  // ---------- stop timer ----------
+  gettimeofday(&Nv, &Nz);
+  after = (double)Nv.tv_sec + (double)Nv.tv_usec * 1.e-6;
+  std::cout << after - before << " sec." << std::endl;
+  // -------------------------------------
+}
+
+
+std::vector<double> biaslist(double N) {
+  std::vector<std::vector<std::vector<std::complex<double>>>> bk(NL, std::vector<std::vector<std::complex<double>>>(NL, std::vector<std::complex<double>>(NL, 0)));
+  int count = 0;
+  double nsigma = sigma*exp(N);
+  std::vector<double> biaslist(NL*NL*NL,0);
+  
+  LOOP{
+    if (innsigma(i,j,k,NL,nsigma,dn)) {
+      bk[i][j][k] = 1;
+      count++;
+    }
+  }
+
+  if (count==0) {
+    return biaslist;
+  }
+  bk /= sqrt(count);
+
+  std::vector<std::vector<std::vector<std::complex<double>>>> biaslattice = fft(bk);
+  LOOP{
+    biaslist[i*NL*NL + j*NL + k] = biaslattice[i][j][k].real();
+  }
+
+  return biaslist;
+}
+
+bool innsigma(int nx, int ny, int nz, int Num, double nsigma, double dn) {
+  int nxt, nyt, nzt; // shifted index
+  if (nx<=Num/2) {
+    nxt = nx;
+  } else {
+    nxt = nx-Num;
+  }
+
+  if (ny<=Num/2) {
+    nyt = ny;
+  } else {
+    nyt = ny-Num;
+  }
+
+  if (nz<=Num/2) {
+    nzt = nz;
+  } else {
+    nzt = nz-Num;
+  }
+
+  return abs(sqrt(nxt*nxt + nyt*nyt + nzt*nzt) - nsigma) <= dn/2.;
+}
