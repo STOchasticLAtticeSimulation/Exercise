897b014e93c6bbeafb802e639883996dcafd1f4c
Yuichiro Tada <yuichiro.t.tada@gmail.com>
2023-10-10 17:07:20 +0900

    biasdata done

--- chaotic/chaotic.cpp
+++ chaotic/chaotic.cpp
@@ -8,7 +8,7 @@ const double dN = 0.01;
 const std::string sourcedir = "../source";
 const std::vector<double> phii{15.,-0.1*mm*mm};
 const double bias = 10.;
-const double Nbias = 3;
+const double Nbias = 3.5;
 const double dNbias = 0.01;
 
 
